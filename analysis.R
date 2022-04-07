7d9820acf33a1e441391bedacfdeb1de3de3d0d8
Stephanie <stephanie.boettiger@outlook.com>
2022-04-07 20:24:59 +0200

    forest plot options, commit from R

--- analysis.R
+++ analysis.R
@@ -8,6 +8,7 @@
 
 library(dplyr)
 library(metafor)
+library(tidyverse)
 
 dat <- read.csv("cmsu_raw-data.csv")
 dat <- dat %>% 
@@ -137,3 +138,75 @@ PET = rma.mv(yi, vi, mods = I(sqrt(vi)),
 PET
 
 
+# forest plot option #1 
+
+forest_df <- meta %>% 
+  arrange(yi) %>% 
+  mutate(
+    study_id = 1:length(yi),
+    lowerci = yi - (1.96*vi),
+    upperci = yi + (1.96*vi),
+  )
+forest_df
+
+forest_df %>% 
+  ggplot(.,
+         aes(x = yi,
+             y = study_id,
+             xmin = lowerci,
+             xmax = upperci)) +
+  geom_point(shape = 15,
+             size = 4, 
+             color = "black"
+  ) + 
+  scale_x_continuous(limits = c(-.4, .6), 
+                     breaks = c(-.4, -.3, -.2, -.1, 0, .1, .2, .3, .4, .5, .6),
+                     name = "Correlation Coefficient (r)") +
+  ylab("Author(s)") + 
+  scale_y_continuous(
+    limits = c(1, length(forest_df$study_id)),
+    breaks = 1:length(forest_df$study_id)
+  ) + 
+  geom_vline(xintercept = 0, 
+             color = "black", 
+             linetype = "dashed")
+
+# forest plot option #2
+
+res2 <- res
+res2$vi.f <- res2$vi.f[1:28]
+
+res3 <- res
+res3$vi.f <- res3$vi.f[29:57]
+res3$yi.f <- res3$yi.f[29:57]
+res3$slab <- res3$slab[29:57]
+forest(res3, xlim = c(-3, 4))
+
+res4 <- res
+res4$vi.f <- res4$vi.f[58:86]
+res4$yi.f <- res4$yi.f[58:86]
+res4$slab <- res4$slab[58:86]
+forest(res4, xlim = c(-3, 4))
+
+res5 <- res
+res5$vi.f <- res4$vi.f[87:115]
+res5$yi.f <- res4$yi.f[87:115]
+res5$slab <- res4$slab[87:115]
+forest(res5, xlim = c(-3, 4))
+
+res6 <- res
+res5$vi.f <- res4$vi.f[116:144]
+res5$yi.f <- res4$yi.f[116:144]
+res5$slab <- res4$slab[116:144]
+
+res7 <- res
+res5$vi.f <- res4$vi.f[145:173]
+res5$yi.f <- res4$yi.f[145:173]
+res5$slab <- res4$slab[145:173]
+forest(res6, xlim = c(-3, 4))
+
+res8 <- res
+res8$vi.f <- res4$vi.f[174:196]
+res8$yi.f <- res4$yi.f[174:196]
+res8$slab <- res4$slab[174:196]
+forest(res6, xlim = c(-3, 4))
